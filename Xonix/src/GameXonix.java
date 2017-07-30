37b1fc6321b09272d8f2a479587107e301edf341
palandr <palandrp@gmail.com>
2017-07-30 23:31:26 +0500

    Подготовил балванку

---
+++ Xonix/src/GameXonix.java
@@ -0,0 +1,25 @@
+/*
+ *  Java game Xonix.
+ *  @author PP
+ *  @version 30.07.17
+ */
+
+import javax.swing.*;
+
+class GameXonix extends JFrame {
+
+    public static void main(String[] args) {
+        new GameXonix().go();
+    }
+
+    GameXonix() {
+           
+    }
+
+    void go() {
+        
+    }
+
+    final String TITLE_OF_PROGRAM = "Xonix";
+    final 
+}
